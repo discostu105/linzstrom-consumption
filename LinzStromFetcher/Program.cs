3b4c804765189a4b70ea4938027d0eff13a8f9d2
Christoph Neumüller <c.neumueller@gmail.com>
2022-02-26 16:11:46 +0100

    init

---
+++ LinzStromFetcher/Program.cs
@@ -0,0 +1,15 @@
+﻿using LinzStromFetcher;
+
+var linzStrom = await LinzStrom.StartSession(
+    username: "",
+    password: ""
+);
+
+
+var csv = await linzStrom.FetchConsumptionAsCsv(
+    dateFrom: "",
+    dateTo: ""
+);
+
+Console.WriteLine(csv.Length);
+
