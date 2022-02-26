937198db350683273bfccfad1a79696ab26028e1
Christoph Neumüller <c.neumueller@gmail.com>
2022-02-27 00:21:21 +0100

    happy coding

--- LinzStromFetcher/Program.cs
+++ LinzStromFetcher/Program.cs
@@ -1,44 +1,78 @@
 ﻿using CommandLine;
+using ConsumptionStorageInfluxDb;
 using LinzNetzApi;
+using Microsoft.Extensions.Configuration;
 using System.Linq;
+using TinyCsvParser;
 
 public class Options {
-    [Option('u', "verbose", Required = true, HelpText = "Linz Strom Username (e-mail).")]
+    [Option('u', "username", Required = false, HelpText = "Linz Strom Username (e-mail).")]
     public string Username { get; set; }
 
-    [Option('p', "password", Required = true, HelpText = "Linz Strom Password.")]
+    [Option('p', "password", Required = false, HelpText = "Linz Strom Password.")]
     public string Password { get; set; }
     
-    [Option('f', "fromdate", Required = false, HelpText = "default -1d. format: dd.MM.yyyy (25.02.2022)")]
-    public string FromDate { get; set; }
+    [Option('d', "days", Required = false, Default = 7, HelpText = "default 7 days)")]
+    public int Days { get; set; }
     
-    [Option('t', "todate", Required = false, HelpText = "default=today. format: dd.MM.yyyy (26.02.2022)")]
-    public string ToDate { get; set; }
+
+    [Option('e', "influxendpoint", Required = false)]
+    public string InfluxDbEndpoint { get; set; }
+
+    [Option('k', "influxtoken", Required = false)]
+    public string InfluxDbToken { get; set; }
 }
 
 class Program {
     public static async Task Main(string[] args) {
+        
         await Parser.Default.ParseArguments<Options>(args)
             .WithParsedAsync<Options>(async o => {
-                if (string.IsNullOrEmpty(o.FromDate)) o.FromDate = DateTime.Now.Subtract(TimeSpan.FromDays(1)).ToString("dd.MM.yyyy");
-                if (string.IsNullOrEmpty(o.ToDate)) o.ToDate = DateTime.Now.ToString("dd.MM.yyyy");
+                var configuration = new ConfigurationBuilder()
+                  .AddUserSecrets<Program>()
+                  .Build();
+
+                if (o.Username == null) o.Username = configuration["LinzNetzUsername"];
+                if (o.Password == null) o.Password = configuration["LinzNetzPassword"];
+                if (o.InfluxDbEndpoint == null) o.InfluxDbEndpoint = configuration["InfluxDbEndpoint"];
+                if (o.InfluxDbToken == null) o.InfluxDbToken = configuration["InfluxDbToken"];
+
                 await Parse(o);
             });
     }
 
     private static async Task Parse(Options options) {
+        var timeout = TimeSpan.FromSeconds(120);
         await using var linzStrom = await LinzNetz.StartSession(
             username: options.Username,
-            password: options.Password
+            password: options.Password,
+            timeout: timeout,
+            headless: true
         );
 
-        var csv = await linzStrom.FetchConsumptionAsCsv(
-            dateFrom: options.FromDate,
-            dateTo: options.ToDate
-        );
+        linzStrom.PrintBaseInfo();
 
-        Console.WriteLine("lines: " + csv.Length);
-        Console.WriteLine("first: " + csv[1]);
-        Console.WriteLine("last: " + csv.Last());
+        var influxdb = await InfluxDb.Connect(options.InfluxDbEndpoint, options.InfluxDbToken, "linzstrom", "home");
+
+        var fromDate = DateTime.Now.Subtract(TimeSpan.FromDays(options.Days)).ToString("dd.MM.yyyy");
+        var toDate = DateTime.Now.ToString("dd.MM.yyyy");
+
+        foreach (var anlage in linzStrom.BaseInfo.anlagen) {
+            var csv = await linzStrom.FetchConsumptionAsCsv(
+                dateFrom: fromDate,
+                dateTo: toDate,
+                anlage: anlage.id
+            );
+
+            CsvParserOptions csvParserOptions = new CsvParserOptions(true, ';');
+            CsvReaderOptions csvReaderOptions = new CsvReaderOptions(new string[] { "\n" });
+
+            var csvParser = new CsvParser<KwhQuarterHourMeasurement>(csvParserOptions, new KwhQuarterHourMeasurementMapping());
+            var records = csvParser.ReadFromString(csvReaderOptions, csv).Select(x => x.Result);
+            Console.WriteLine("writing " + records.Count() + " csv records");
+            Console.WriteLine("first: " + records.First());
+            Console.WriteLine("last: " + records.Last());
+            await influxdb.WriteKwhQuarterHourMeasurements(records, anlage.name);
+        }
     }
 }
---
+++ LinzStromFetcher/readme.md
@@ -0,0 +1,10 @@
+﻿
+# Secrets
+
+```
+dotnet user-secrets init
+dotnet user-secrets set "InfluxDbToken" "xxx"
+dotnet user-secrets set "InfluxDbEndpoint" "xxx"
+dotnet user-secrets set "LinzNetzUsername" "xxx"
+dotnet user-secrets set "LinzNetzPassword" "xxx"
+```
